a698bf36cb52c40470da90f5fa707149f567c21e
Lumi K <149065128+lkilpela@users.noreply.github.com>
2024-08-18 10:30:44 +0000

    chore: Update test.txt and replace.cpp for ex04

--- ex04/replace.cpp
+++ ex04/replace.cpp
@@ -6,7 +6,7 @@
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/16 09:42:01 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/08/18 09:27:00 by lkilpela         ###   ########.fr       */
+/*   Updated: 2024/08/18 09:34:31 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,7 @@ std::string replaceAll(const std::string& str, const std::string& s1, const std:
     size_t pos = 0; // position of found subtring
     size_t prevPos = 0; // position of the previous substring
 
-    pos = str.find(s1, prevPos);
-    while(pos != std::string::npos) {
+    while((pos = str.find(s1, prevPos)) != std::string::npos) {
         result.append(str, prevPos, pos - prevPos);
         result.append(s2);
         prevPos = pos + s1.length();

744d77929e693f28e52f15956ce342742a861ac9
Lumi K <149065128+lkilpela@users.noreply.github.com>
2024-08-19 12:11:22 +0000

    Update replace.cpp to handle empty files and improve error handling

--- CPP01/ex04/replace.cpp
+++ CPP01/ex04/replace.cpp
@@ -6,7 +6,7 @@
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/16 09:42:01 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/08/19 12:05:43 by lkilpela         ###   ########.fr       */
+/*   Updated: 2024/08/19 12:10:07 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,13 +40,10 @@ void    replaceInfile(const std::string& filename, const std::string& s1, const 
         return ;
     }
 
-    if (!std::getline(inputFile, line)) {
-    std::cerr << "Error: The file " << filename << " is empty or could not be read" << std::endl;
-    return;
+    if (inputFile.peek() == std::ifstream::traits_type::eof()) {
+        std::cerr << "Error: The file " << filename << " is empty" << std::endl;
+        return;
     }
-
-    // Process the first line
-    outputFile << replaceAll(line, s1, s2) << "\n";
     
     while (std::getline(inputFile, line)) {
         outputFile << replaceAll(line, s1, s2) << "\n";

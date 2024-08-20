9b94a049e067b3c2fc49fb201853b8cf34687494
Lumi K <149065128+lkilpela@users.noreply.github.com>
2024-08-20 20:16:43 +0000

    Update ClapTrap class implementation and header file

---
+++ CPP03/ex00/ClapTrap.hpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/20 20:00:58 by lkilpela          #+#    #+#             */
+/*   Updated: 2024/08/20 20:12:06 by lkilpela         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLAPTRAP_HPP
+# define CLAPTRAP_HPP
+
+#include <iostream>
+#include <string>
+
+class ClapTrap {
+private:
+    std::string name;
+    unsigned int hitPoints;
+    unsigned int energyPoints;
+    unsigned int attackDamage;
+
+public:
+    ClapTrap(const std::string& name);
+    ClapTrap(const ClapTrap& other);
+    ClapTrap& operator=(const ClapTrap& other);
+    ~ClapTrap();
+    
+    void attack(const std::string& target);
+    void takeDamage(unsigned int amount);
+    void beRepaired(unsigned int amount);
+};
+
+#endif

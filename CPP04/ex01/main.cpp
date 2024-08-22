e1ae7ccd75c77e26939461d027046e8327c9483f
Lumi K <149065128+lkilpela@users.noreply.github.com>
2024-08-22 18:55:04 +0000

    refactor: Update Animal, Cat, and Dog classes

--- CPP04/ex01/main.cpp
+++ CPP04/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/22 07:18:37 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/08/22 18:17:26 by lkilpela         ###   ########.fr       */
+/*   Updated: 2024/08/22 18:54:19 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,33 +30,42 @@ void testDogDeepCopy() {
 
     Dog copiedDog = originalDog; // Using copy constructor
 
+    std::cout << GREEN << "~Modifying Original..." << RESET << std::endl;
     // Modify the original Dog's Brain
     originalDog.getBrain()->ideas[0] = "Modified idea";
 
+    std::cout << GREEN << "~Checking Copy..." << RESET << std::endl;
     // Check if the copied Dog's Brain is unaffected
     if (copiedDog.getBrain()->ideas[0] == "Original idea") {
         std::cout << "Dog deep copy test passed." << std::endl;
     } else {
         std::cout << "Dog deep copy test failed." << std::endl;
     }
+    std::cout << GREEN << "~End of Dog Deep Copy Test" << RESET << std::endl;
 }
 
 void testCatDeepCopy() {
     std::cout << GREEN << "~Calling Constructor..." << RESET << std::endl;
     Cat originalCat;
+
+    std::cout << GREEN << "\n~Getting Copy..." << RESET << std::endl;
     originalCat.getBrain()->ideas[0] = "Original idea";
 
     Cat copiedCat = originalCat; // Using copy constructor
 
+    std::cout << GREEN << "~Modifying Original..." << RESET << std::endl;
     // Modify the original Cat's Brain
     originalCat.getBrain()->ideas[0] = "Modified idea";
 
+    std::cout << GREEN << "~Checking Copy..." << RESET << std::endl;
     // Check if the copied Cat's Brain is unaffected
     if (copiedCat.getBrain()->ideas[0] == "Original idea") {
         std::cout << "Cat deep copy test passed." << std::endl;
     } else {
         std::cout << "Cat deep copy test failed." << std::endl;
     }
+    std::cout << GREEN << "~End of Cat Deep Copy Test" << RESET << std::endl;
+    
 }
 
 void testDogCopyInScope() {
@@ -64,12 +73,14 @@ void testDogCopyInScope() {
     Dog basic;
     {
         Dog tmp = basic; // Using copy constructor
+        std::cout << GREEN << "~Scope Ends for tmp" << RESET << std::endl;
     } // tmp goes out of scope here
+    std::cout << GREEN << "~End of Dog Deep In Scope" << RESET << std::endl;
 }
 
 int main()
 {
-    std::cout << BLUE << "Creating and filling an array of Animal objects..." << RESET << std::endl;
+    std::cout << BLUE << "\nCalling Constructor to Create and Fill the Array of Animal Objects..." << RESET << std::endl;
     const int numAnimals = 10;
     Animal* animals[numAnimals];
 
@@ -77,20 +88,22 @@ int main()
     for (int i = 0; i < numAnimals / 2; ++i) {
         animals[i] = new Dog();
     }
+    std::cout << BLUE << "...Half Dog created..." << RESET << std::endl;
     for (int i = numAnimals / 2; i < numAnimals; ++i) {
         animals[i] = new Cat();
     }
+    std::cout << BLUE << "...Half Cat created..." << RESET << std::endl;
 
     std::cout << BLUE << "\nTesting Dog Deep Copy..." << RESET << std::endl;
     testDogDeepCopy();
-
+        
     std::cout << BLUE << "\nTesting Cat Deep Copy..." << RESET << std::endl;
     testCatDeepCopy();
-
+    
     std::cout << BLUE << "\nTesting Dog Copy In Scope..." << RESET << std::endl;
     testDogCopyInScope();
     
-    std::cout << GREEN << "\n~Calling Destructor..." << RESET << std::endl;
+    std::cout << GREEN << "\n~Calling Destructor to Delete the Array of Animal Objects..." << RESET << std::endl;
     // Delete all animals
     for (int i = 0; i < numAnimals; ++i) {
         delete animals[i];

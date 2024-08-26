8e08efdcf8ea079f71cbfea46f2631ea0964fc1c
Lumi K <149065128+lkilpela@users.noreply.github.com>
2024-08-26 09:50:21 +0000

    chore: Refactor Weapon class constructor and destructor

--- CPP01/ex03/Weapon.cpp
+++ CPP01/ex03/Weapon.cpp
@@ -6,13 +6,17 @@
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/19 11:27:05 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/08/20 19:44:10 by lkilpela         ###   ########.fr       */
+/*   Updated: 2024/08/26 09:45:48 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Weapon.hpp"
 
+Weapon::Weapon() : type("default") {}
+
 Weapon::Weapon(const std::string& type) : type(type) {};
+
+Weapon::~Weapon() {}
 
 void Weapon::setType(const std::string& newType) {
     type = newType;
--- CPP01/ex03/Weapon.hpp
+++ CPP01/ex03/Weapon.hpp
@@ -6,7 +6,7 @@
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/15 12:02:19 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/08/20 19:43:23 by lkilpela         ###   ########.fr       */
+/*   Updated: 2024/08/26 09:43:26 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,10 @@ class Weapon {
 private:
     std::string type;
 public:
+    Weapon();
     // Constructor accepting std::string
     Weapon(const std::string& type);
+    ~Weapon();
     
     void setType(const std::string& newType);
     const std::string& getType() const;

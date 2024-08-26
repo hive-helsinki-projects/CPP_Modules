e5c1a48e889ed4c3a9fbfacd45bec804eb529d6a
Lumi K <149065128+lkilpela@users.noreply.github.com>
2024-08-26 09:54:44 +0000

    chore: Update HumanA.hpp and HumanB.hpp with necessary includes

--- CPP01/ex03/HumanA.hpp
+++ CPP01/ex03/HumanA.hpp
@@ -6,13 +6,15 @@
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/15 12:08:48 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/08/20 19:42:09 by lkilpela         ###   ########.fr       */
+/*   Updated: 2024/08/26 09:53:27 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef HUMANA_HPP
 #define HUMANA_HPP
 
+#include <iostream>
+#include <string>
 #include "Weapon.hpp"
 
 class HumanA {
--- CPP01/ex03/HumanB.hpp
+++ CPP01/ex03/HumanB.hpp
@@ -6,13 +6,15 @@
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/15 12:08:48 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/08/20 19:42:36 by lkilpela         ###   ########.fr       */
+/*   Updated: 2024/08/26 09:53:17 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef HUMANB_HPP
 #define HUMANB_HPP
 
+#include <iostream>
+#include <string>
 #include "Weapon.hpp"
 
 class HumanB {

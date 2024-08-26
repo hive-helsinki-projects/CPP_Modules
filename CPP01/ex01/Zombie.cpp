ac7826049920eecf5ab06e2809f6b51c901a2fda
Lumi K <149065128+lkilpela@users.noreply.github.com>
2024-08-26 09:37:51 +0000

    feat: Add Zombie class implementation

--- CPP01/ex01/Zombie.cpp
+++ CPP01/ex01/Zombie.cpp
@@ -6,7 +6,7 @@
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/26 12:11:38 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/08/26 12:15:18 by lkilpela         ###   ########.fr       */
+/*   Updated: 2024/08/26 09:34:24 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ Zombie::Zombie() : name("default") {} // Default constructor
 Zombie::Zombie(std::string name) : name(name) {} // Initializer List 
 
 Zombie::~Zombie() {
-    std::cout << name << " is destroyed." << "\n";
+    std::cout << name << " is destroyed.\n";
 }
 void Zombie::announce(void) {
     std::cout << name << ": BraiiiiiiinnnzzzZ..." << "\n";
--- CPP01/ex01/main.cpp
+++ CPP01/ex01/main.cpp
@@ -6,19 +6,19 @@
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/15 10:44:53 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/08/26 12:15:18 by lkilpela         ###   ########.fr       */
+/*   Updated: 2024/08/26 09:35:47 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ZombieHorde.hpp"
+#include "Zombie.hpp"
 
 int main() {
     int N = 5;
     std::string name = "ZombieHorde";
     Zombie* horde = zombieHorde(N, name);
     if (horde) {
-    for (int i = 0; i < N; ++i) {
-        horde[i].announce();
+        for (int i = 0; i < N; ++i) {
+            horde[i].announce();
     }
     delete[] horde;
     } else {

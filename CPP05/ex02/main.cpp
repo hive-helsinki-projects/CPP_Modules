e74a7f200b12ab9eb1b47c4509f9c1b45d71faa5
lumikil <kilpelainenlumi@gmail.com>
2024-10-17 14:56:10 +0300

    Enhance main function: Add numbered sections for form status and bureaucrat actions, improve exception handling

--- CPP05/ex02/main.cpp
+++ CPP05/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 22:03:50 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/10/17 14:50:09 by lkilpela         ###   ########.fr       */
+/*   Updated: 2024/10/17 14:56:00 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,18 +27,18 @@ int main() {
 
     try {
       
-        std::cout << BLUE << "\n[STATUS OF FORMS]" << RESET << std::endl;
+        std::cout << BLUE << "\n[1. STATUS OF FORMS]" << RESET << std::endl;
         ShrubberyCreationForm form("Shrubbery");
         RobotomyRequestForm form1("Robotomy");
         PresidentialPardonForm form2("Presidential");
         printFormStatus(form, form1, form2);
 
-        std::cout << BLUE << "\n[BUREAUCRAT ALICE]" << RESET << std::endl;
+        std::cout << BLUE << "\n[1.BUREAUCRAT ALICE]" << RESET << std::endl;
         Bureaucrat alice("Alice", 1); // Grade 1 (highest), grade 150 (lowest)
         std::cout << alice << std::endl;
         
         std::cout << GREEN << alice.getName() << " is signning the form..." << std::endl << RESET;        
-        alice.signForm(form); // Form is not signed, throws exception
+        //alice.signForm(form); // Form is not signed, throws exception
         alice.executeForm(form);  // Should fail      
 
         alice.signForm(form1); // Form is signed
@@ -50,7 +50,37 @@ int main() {
         std::cout << std::endl;
         printFormStatus(form, form1, form2);
 
-        std::cout << BLUE << "\n[DESTRUCTOR]" << std::endl << RESET;
+        std::cout << BLUE << "\n[1. DESTRUCTOR]" << std::endl << RESET;
+    } catch (const std::exception& e) {
+        std::cerr << "Exception: " << e.what() << std::endl;
+    }
+
+    try {
+        
+        std::cout << BLUE << "\n[2. STATUS OF FORMS]" << RESET << std::endl;
+        ShrubberyCreationForm form("Shrubbery");
+        RobotomyRequestForm form1("Robotomy");
+        PresidentialPardonForm form2("Presidential");
+        printFormStatus(form, form1, form2);
+
+        std::cout << BLUE << "\n[2. BUREAUCRAT BOB]" << RESET << std::endl;
+        Bureaucrat bob("Bob", 149); // Grade 1 (highest), grade 150 (lowest)
+        std::cout << bob << std::endl;
+        
+        std::cout << GREEN << bob.getName() << " is signning the form..." << std::endl << RESET;        
+        bob.signForm(form); // Should fail, throws exception
+        bob.executeForm(form);  // Should fail, throws exception      
+
+        bob.signForm(form1); // Should fail, throws exception
+        bob.executeForm(form1); // Should fail, throws exception
+        
+        bob.signForm(form2); // Should fail, throws exception
+        bob.executeForm(form2); // Should fail, throws exception
+
+        std::cout << std::endl;
+        printFormStatus(form, form1, form2);
+
+        std::cout << BLUE << "\n[2. DESTRUCTOR]" << std::endl << RESET;
     } catch (const std::exception& e) {
         std::cerr << "Exception: " << e.what() << std::endl;
     }

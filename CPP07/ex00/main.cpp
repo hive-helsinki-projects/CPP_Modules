0dc7d3eff3628c56e6f1faefc601759874da2a3f
lumikil <kilpelainenlumi@gmail.com>
2024-10-23 13:18:24 +0300

    Add commented-out Awesome class and example usage in main function

--- CPP07/ex00/main.cpp
+++ CPP07/ex00/main.cpp
@@ -6,13 +6,33 @@
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 12:08:29 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/10/23 12:08:51 by lkilpela         ###   ########.fr       */
+/*   Updated: 2024/10/23 13:18:11 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "whatever.hpp"
 #include <iostream>
+#include <string>
 
+/* class Awesome
+{
+public:
+    Awesome(void) : _n(0) {}
+    Awesome( int n ) : _n( n ) {}
+    Awesome(const Awesome& other) : _n(other._n) {}
+    Awesome& operator= (Awesome& a) { _n = a._n; return *this; }
+    bool operator==( Awesome const & rhs ) const { return (this->_n == rhs._n); }
+    bool operator!=( Awesome const & rhs ) const{ return (this->_n != rhs._n); }
+    bool operator>( Awesome const & rhs ) const { return (this->_n > rhs._n); }
+    bool operator<( Awesome const & rhs ) const { return (this->_n < rhs._n); }
+    bool operator>=( Awesome const & rhs ) const { return (this->_n >= rhs._n); }
+    bool operator<=( Awesome const & rhs ) const { return (this->_n <= rhs._n); }
+    int get_n() const { return _n; }
+private:
+    int _n;
+};
+std::ostream & operator<<(std::ostream & o, const Awesome &a) { o << a.get_n(); return o; }
+ */
 int main( void )
 {
     int a = 2;
@@ -27,5 +47,11 @@ int main( void )
     std::cout << "c = " << c << ", d = " << d << std::endl;
     std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
     std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
-    return 0;
+/*     
+    Awesome a(2), b(4);
+    swap(a, b);
+    std::cout << a << " " << b << std::endl;
+    std::cout << max(a, b) << std::endl;
+    std::cout << min(a, b) << std::endl;
+    return 0; */
 }

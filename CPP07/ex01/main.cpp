5dbfe32dbf33f86715af2251ac3bf192da13a240
lumikil <kilpelainenlumi@gmail.com>
2024-10-23 13:05:24 +0300

    Add tests for iter function with empty array and array of pointers

--- CPP07/ex01/main.cpp
+++ CPP07/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 12:32:38 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/10/23 12:43:54 by lkilpela         ###   ########.fr       */
+/*   Updated: 2024/10/23 13:05:15 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,11 @@
 
 int main()
 {
+    // Test with empty array
+    int empty[] = {};
+    iter(empty, 0, print);
+    std::cout << std::endl;
+    
     // Test with int array
     int array[] = {1, 2, 3, 4, 5};
     iter(array, 5, print);
@@ -33,6 +38,12 @@ int main()
     // Test with double array
     double array4[] = {1.1, 2.2, 3.3, 4.4, 5.5};
     iter(array4, 5, print);
+    std::cout << std::endl;
+    
+    // Test with array of pointers
+    int a = 1, b = 2, c = 3;
+    int *array5[] = {&a, &b, &c};
+    iter(array5, 3, print);
     std::cout << std::endl;
     
     return 0;

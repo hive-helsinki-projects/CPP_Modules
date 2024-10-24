f7f3bea06b8ececbb66995fe82d5331f734d374e
lumikil <kilpelainenlumi@gmail.com>
2024-10-24 15:34:05 +0300

    Enhance Span class documentation: clarify shortest and longest span calculations with examples; fix loop condition in shortestSpan method

--- CPP08/ex01/Span.cpp
+++ CPP08/ex01/Span.cpp
@@ -6,7 +6,7 @@
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 15:23:10 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/10/24 13:54:27 by lkilpela         ###   ########.fr       */
+/*   Updated: 2024/10/24 15:01:16 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,7 +40,15 @@ void Span::addNumber(int number) {
     numbers.push_back(number);
 }
 
-/* FIND SHORTEST SPAN (MIN DISTANCE BETWEEN TWO NUMBERS) */
+/* FIND SHORTEST SPAN (MIN DISTANCE BETWEEN TWO CONSECUTIVE NUMBERS) 
+** Given numbers : 6, 3, 17, 9, 11
+** Sorted numbers: 3, 6, 9, 11, 17
+** Span1 = 6 - 3 = 3
+** Span2 = 9 - 6 = 3
+** Span3 = 11 - 9 = 2
+** Span4 = 17 - 11 = 6
+** Shortest span = 2
+*/
 int Span::shortestSpan() const {
     if (numbers.size() < 2) {
         throw std::logic_error("Not enough numbers to calculate span");
@@ -48,7 +56,7 @@ int Span::shortestSpan() const {
     std::vector<int> sorted = numbers;
     std::sort(sorted.begin(), sorted.end());
     int min = std::numeric_limits<int>::max();
-    for (size_t i = 1; i < sorted.size() - 1; i++) {
+    for (size_t i = 1; i < sorted.size(); i++) {
         int span = sorted[i] - sorted[i - 1];
         if (span < min) {
             min = span;
@@ -57,7 +65,12 @@ int Span::shortestSpan() const {
     return min;
 }
 
-/* FIND LONGEST SPAN (MAX DISTANCE BETWEEN TWO NUMBERS) */
+/* FIND LONGEST SPAN (MAX DISTANCE BETWEEN SMALLEST AND LARGEST NUMBERS)
+** Given numbers : 6, 3, 17, 9, 11
+** Sorted numbers: 3, 6, 9, 11, 17
+** Min = 3, Max = 17
+** Longest span = 17 - 3 = 14
+*/
 int Span::longestSpan() const {
     if (numbers.size() < 2) {
         throw std::logic_error("Not enough numbers to calculate span");

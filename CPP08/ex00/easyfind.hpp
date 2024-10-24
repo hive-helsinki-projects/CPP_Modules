fd491f2ce2035e07c1242807a42f8a82a7fb46ae
lumikil <kilpelainenlumi@gmail.com>
2024-10-24 11:22:34 +0300

    Refactor easyfind function for improved readability and added comments

--- CPP08/ex00/easyfind.hpp
+++ CPP08/ex00/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 14:42:19 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/10/23 15:19:34 by lkilpela         ###   ########.fr       */
+/*   Updated: 2024/10/24 11:22:23 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,23 @@
 # include <algorithm>
 # include <stdexcept>
 
-template <typename T>
+// Template for the container type.
+template <typename T> 
+
+// Function that searches for a value in a container.
 int easyfind(T& container, int value) {
-    typename T::iterator it = std::find(container.begin(), container.end(), value);
-    if (it != container.end()) {
+    // Initializes begin to the iterator pointing to the start of the container.
+    auto begin = container.begin();
+    
+    // Initializes end to the iterator pointing to the end of the container
+    auto end = container.end();
+    
+    // Uses std::find to search for the value between begin and end,
+    // and initializes it to the result.
+    auto it = std::find(begin, end, value);
+
+    // If the value is found, return the value.
+    if (it != end) {
         return *it;
     } else {
         throw std::runtime_error("Value not found in container");

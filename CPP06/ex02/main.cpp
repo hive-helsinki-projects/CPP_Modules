b4771aade87c85645b88084613a1f331288d7815
lumikil <kilpelainenlumi@gmail.com>
2024-11-18 12:55:38 +0200

    Refactor main.cpp: improve random number generation and exception handling in identify function

--- CPP06/ex02/main.cpp
+++ CPP06/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/18 22:54:52 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/10/23 11:44:37 by lkilpela         ###   ########.fr       */
+/*   Updated: 2024/11/18 12:55:33 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,10 @@
 #include <iostream> // std::cout, std::endl
 #include <ctime> // time
 #include <cstdlib> // srand, rand
-#include <typeinfo> // std::bad_cast 
 #include "D.hpp"
 
 // Generate a random class
 Base* generate(void) {
-    srand(static_cast<unsigned int>(time(0)));
     int random = rand() % 4;
     switch (random) {
         case 0:
@@ -34,7 +32,7 @@ Base* generate(void) {
         case 2:
             std::cout << "Generated C" << std::endl;
             return new C();
-        case 3: // Dummy class that is not derived from Base. This class is not handled by the identify functions, so it will trigger "Unknown type" message.
+        case 3: // This class is not handled by the identify functions, so it will trigger "Unknown type" message.
             std::cout << "Generated D" << std::endl;
             return new D();
     }
@@ -59,22 +57,28 @@ void identify(Base& p) {
     try {
         (void)dynamic_cast<A&>(p);
         std::cout << "A" << std::endl;
-    } catch (std::bad_cast&) {}
+        return ;
+    } catch (...) {}
 
     try {
         (void)dynamic_cast<B&>(p);
         std::cout << "B" << std::endl;
-    } catch (std::bad_cast&) {}
+        return ;
+    } catch (...) {}
 
     try {
         (void)dynamic_cast<C&>(p);
         std::cout << "C" << std::endl;
-    } catch (std::bad_cast&) {}
+        return ;
+    } catch (...) {}
 
     std::cout << "Unknown type" << std::endl;
 }
 
 int main() {
+    // Seed the random number generator
+    srand(static_cast<unsigned int>(time(0)));
+    
     Base* basePtr = generate();
     std::cout << "Identifying by pointer: ";
     identify(basePtr);

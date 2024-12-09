b36e57ea8286efaf9bf3f3c564c9b38b641514e6
lkilpela <lkilpela@student.hive.fi>
2024-12-09 18:26:06 +0200

    Refactor main.cpp: Update MutantStack and add list test

--- CPP08/ex02/main.cpp
+++ CPP08/ex02/main.cpp
@@ -6,18 +6,19 @@
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 15:39:07 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/12/09 17:33:37 by lkilpela         ###   ########.fr       */
+/*   Updated: 2024/12/09 18:25:48 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "MutantStack.hpp"
 #include <iostream>
+#include <list>
 
-int main()
+void basicTest(MutantStack<int>& mstack)
 {
     std::cout << GREEN "Basic Test" RESET << std::endl; 
-    MutantStack<int> mstack;
-    mstack.push(5);
+
+    mstack.push(5); // Adds element to top of stack
     mstack.push(17);
     
     std::cout << "Top: " << mstack.top() << std::endl;
@@ -25,7 +26,7 @@ int main()
     mstack.pop(); // Removes the top element
     std::cout << "Size: " << mstack.size() << std::endl;
     
-    mstack.push(3);
+    mstack.push(3); 
     mstack.push(5);
     mstack.push(737);
     mstack.push(0);
@@ -33,6 +34,33 @@ int main()
     std::cout << "Iterating through stack:" << std::endl;
     MutantStack<int>::iterator it = mstack.begin();
     MutantStack<int>::iterator ite = mstack.end();
+    ++it; // Skip first element
+    --it; // Reset to first element
+    while (it != ite)
+    {
+        std::cout << *it << std::endl;
+        ++it;
+    }
+}
+void testList(std::list<int> list)
+{
+    std::cout << GREEN "List Test" RESET << std::endl;
+    list.push_back(5);
+    list.push_back(17);
+    
+    std::cout << "Back: " << list.back() << std::endl;
+    
+    list.pop_back();
+    std::cout << "Size: " << list.size() << std::endl;
+    
+    list.push_back(3);
+    list.push_back(5);
+    list.push_back(737);
+    list.push_back(0);
+
+    std::cout << "Iterating through list:" << std::endl;
+    std::list<int>::iterator it = list.begin();
+    std::list<int>::iterator ite = list.end();
     ++it;
     --it;
     while (it != ite)
@@ -40,47 +68,16 @@ int main()
         std::cout << *it << std::endl;
         ++it;
     }
-    std::stack<int> s(mstack);
+}
+
+int main()
+{
+
+    MutantStack<int> mstack;
+    basicTest(mstack);
+
+    std::list<int> list;
+    testList(list);
     
-    // Test copy constructor
-    std::cout << GREEN "\nTest copy constructor" RESET << std::endl;
-    MutantStack<int> copyStack(mstack);
-    std::cout << "Copy stack contents:" << std::endl;
-    for (auto it = copyStack.begin(); it != copyStack.end(); ++it)
-    {
-        std::cout << *it << std::endl;
-    }
-
-    // Test assignment operator
-    std::cout << GREEN "\nTest assignment operator" RESET << std::endl;
-    MutantStack<int> assignStack;
-    assignStack = mstack;
-    std::cout << "Assigned stack contents:" << std::endl;
-    for (auto it = assignStack.begin(); it != assignStack.end(); ++it)
-    {
-        std::cout << *it << std::endl;
-    }
-
-    // Test with different data types
-    std::cout << GREEN "\nTest with different data types" RESET << std::endl;
-    MutantStack<std::string> stringStack;
-    stringStack.push("Hello");
-    stringStack.push("World");
-    std::cout << "String stack contents:" << std::endl;
-    for (auto it = stringStack.begin(); it != stringStack.end(); ++it)
-    {
-        std::cout << *it << std::endl;
-    }
-
-    // Test with standard stack
-    std::cout << GREEN "\nTest with standard stack" RESET << std::endl;
-    std::stack<int> s1(mstack);
-    std::cout << "Standard stack contents:" << std::endl;
-    while (!s1.empty())
-    {
-        std::cout << s1.top() << std::endl;
-        s1.pop();
-    }
-
     return 0;
 }

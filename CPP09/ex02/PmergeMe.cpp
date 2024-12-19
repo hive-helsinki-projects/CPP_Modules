5f30467c0b404b74bd9e2d20a229700532076f02
lumikil <kilpelainenlumi@gmail.com>
2024-12-19 19:39:59 +0200

    Add detailed comments for Ford-Johnson algorithm execution steps in PmergeMe

--- CPP09/ex02/PmergeMe.cpp
+++ CPP09/ex02/PmergeMe.cpp
@@ -10,6 +10,33 @@
 4. Insert any remaining elements into the merged list.*/
 
 // FORD-JOHSON ALGORITM
+
+/*
+Initial Vector [3, 6, 8, 2]
+Step-by-Step Execution
+1. Initial Call to mergeInsertSort
+- mergeInsertSort(vec) is called with vec = [3, 6, 8, 2].
+2. First Call to fordJohnsonSort
+- fordJohnsonSort(vec, 0, 3) is called.
+- mid = 0 + (3 - 0) / 2 = 1.
+3. Recursive Calls to fordJohnsonSort
+- fordJohnsonSort(vec, 0, 1) is called.
+    mid = 0 + (1 - 0) / 2 = 0.
+    fordJohnsonSort(vec, 0, 0) is called and returns immediately (base case).
+    fordJohnsonSort(vec, 1, 1) is called and returns immediately (base case).
+    merge(vec, 0, 0, 1) is called to merge [3] and [6].
+        -Result after merge: [3, 6, 8, 2].
+- fordJohnsonSort(vec, 2, 3) is called.
+    mid = 2 + (3 - 2) / 2 = 2.
+    fordJohnsonSort(vec, 2, 2) is called and returns immediately (base case).
+    fordJohnsonSort(vec, 3, 3) is called and returns immediately (base case).
+    merge(vec, 2, 2, 3) is called to merge [8] and [2].
+        - Result after merge: [3, 6, 2, 8].
+4. Final Merge
+merge(vec, 0, 1, 3) is called to merge [3, 6] and [2, 8].
+    - Result after merge: [2, 3, 6, 8].
+*/
+
 void merge(std::vector<int>& vec, int left, int mid, int right) {
     std::vector<int> leftVec(vec.begin() + left, vec.begin() + mid + 1);
     std::vector<int> rightVec(vec.begin() + mid + 1, vec.begin() + right + 1);
@@ -39,6 +66,7 @@ void fordJohnsonSort(std::vector<int>& vec, int left, int right) {
     if (left >= right) return;
 
     int mid = left + (right - left) / 2;
+    
     fordJohnsonSort(vec, left, mid);
     fordJohnsonSort(vec, mid + 1, right);
 
